644b6b78d6a8da6c6025b97ee3f4d62688a7d8eb
Matt Alleman <rmalleman@gmail.com>
2017-02-16 11:58:24 -0700

    added bootstrap script

--- bin/create-bootstrap-script.sh
+++ bin/create-bootstrap-script.sh
@@ -1,4 +1,5 @@
  coursier bootstrap \
-    com.geirsson:scala-db-codegen_2.11:0.2.3 \
+    com.geirsson:scala-db-codegen_2.11:0.2.4 \
+    -r https://maven.internal.artemishealth.com/artifactory/libs-release \
     -f -o scala-db-codegen \
     -M com.geirsson.codegen.Codegen
